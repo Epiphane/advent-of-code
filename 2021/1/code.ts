8a53a97d4d0f59576d4a1cc4929bbb707a8e4425
Thomas Steinke <tsteinke@riotgames.com>
2021-11-30 21:11:11 -0800

    polish

--- 2021/1/code.ts
+++ 2021/1/code.ts
@@ -1,11 +1,4 @@
 import * as fs from "fs";
-import { Map, MapFromInput } from "../../map";
-// import { MakeGrid, MakeRow } from '../../makegrid.js';
-import { permute, gcd, lcm } from "../../utils";
-import { question } from "readline-sync";
-const md5 = require("../../md5");
-const print = console.log;
-
 let file = process.argv[2] || "input";
 let raw = fs
   .readFileSync(file + ".txt")
@@ -36,14 +29,14 @@ let asNumbers = raw.split("\n").map((line) => parseInt(line.trim()));
 // print(result);
 
 // I'm very cool and clever
-print(
+console.log(
   `Part 1: ${asNumbers.reduce(
     (prev, n, i) => prev + (i > 0 && n > asNumbers[i - 1] ? 1 : 0),
     0
   )}`
 );
 
-print(
+console.log(
   `Part 2: ${asNumbers.reduce(
     (prev, n, i) => prev + (i > 2 && n > asNumbers[i - 3] ? 1 : 0),
     0
