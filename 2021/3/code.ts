f1bf56790157b51f33f2afd62be2ae2fc61e2b1b
Thomas Steinke <tsteinke@riotgames.com>
2021-12-03 01:03:51 -0800

    cleanup

--- 2021/3/code.ts
+++ 2021/3/code.ts
@@ -1,105 +1,44 @@
 import * as fs from "fs";
-import { Map, MapFromInput } from "../../map";
-// import { MakeGrid, MakeRow } from '../../makegrid.js';
-import { permute, gcd, lcm } from "../../utils";
-import { question } from "readline-sync";
-const md5 = require("../../md5");
-const print = console.log;
 
 let file = process.argv[2] || "input";
 let raw = fs
   .readFileSync(file + ".txt")
   .toString()
   .trim();
+let input = raw.split("\n").map((line) => line.trim());
 
-let asLines = raw.split("\n").map((line) => line.trim());
-let asNumbers = raw.split("\n").map((line) => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map((line) =>
-  line
-    .trim()
-    .split("\n")
-    .map((line) => line.trim())
-);
-let asMap = MapFromInput(".");
-let asNumberMap = MapFromInput(0, (s) => parseInt(s));
-
-let gamma = "";
-let epsilon = "";
-let totalZero = [];
-let totalOne = [];
-asLines.forEach((line) => {
-  line.split("").forEach((val, i) => {
-    totalZero[i] = totalZero[i] || 0;
-    totalOne[i] = totalOne[i] || 0;
-    if (val === "1") {
-      totalOne[i]++;
-    } else {
-      totalZero[i]++;
-    }
-  });
-});
-
-totalOne.forEach((v, i) => {
-  gamma += v >= totalZero[i] ? "1" : "0";
-  epsilon += v >= totalZero[i] ? "0" : "1";
-});
-
-console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
-
-let oxygen = 0;
-let co2 = 0;
-
-let copy = [...asLines];
-let copy2 = [...asLines];
-
-for (let i = 0; i < asLines[0].length; i++) {
-  if (copy.length > 1) {
-    let totalZero = [];
-    let totalOne = [];
-    copy.forEach((line) => {
-      line.split("").forEach((val, i) => {
-        totalZero[i] = totalZero[i] || 0;
-        totalOne[i] = totalOne[i] || 0;
-        if (val === "1") {
-          totalOne[i]++;
-        } else {
-          totalZero[i]++;
-        }
-      });
-    });
-
-    copy = copy.filter((val) => {
-      if (totalOne[i] >= totalZero[i]) {
-        return val[i] === "1";
-      } else {
-        return val[i] === "0";
-      }
-    });
-    console.log(i, copy, totalOne[i] >= totalZero[i]);
-  }
-
-  if (copy2.length > 1) {
-    let totalZero = [];
-    let totalOne = [];
-    copy2.forEach((line) => {
-      line.split("").forEach((val, i) => {
-        totalZero[i] = totalZero[i] || 0;
-        totalOne[i] = totalOne[i] || 0;
-        if (val === "1") {
-          totalOne[i]++;
-        } else {
-          totalZero[i]++;
-        }
-      });
-    });
-    copy2 = copy2.filter((val) => {
-      if (totalOne[i] >= totalZero[i]) {
-        return val[i] === "0";
-      } else {
-        return val[i] === "1";
-      }
-    });
-  }
+function gamma(numbers: string[]) {
+  return numbers[0]
+    .split("")
+    .map((_, i) =>
+      numbers.filter((n) => n[i] === "1").length >= numbers.length / 2
+        ? "1"
+        : "0"
+    )
+    .join("");
 }
-console.log(copy, copy2);
-console.log(parseInt(copy[0], 2) * parseInt(copy2[0], 2));
+
+function inverse(number: string) {
+  return number
+    .split("")
+    .map((i) => (i === "1" ? "0" : "1"))
+    .join("");
+}
+
+console.log(
+  `Part 1:`,
+  parseInt(gamma(input), 2) * parseInt(inverse(gamma(input)), 2)
+);
+
+let oxygen = [...input];
+for (let i = 0; oxygen.length > 1; i++) {
+  const match = gamma(oxygen);
+  oxygen = oxygen.filter((num) => num[i] === match[i]);
+}
+
+let co2 = [...input];
+for (let i = 0; co2.length > 1; i++) {
+  const match = inverse(gamma(co2));
+  co2 = co2.filter((num) => num[i] === match[i]);
+}
+console.log(`Part 2:`, parseInt(oxygen[0], 2) * parseInt(co2[0], 2));
