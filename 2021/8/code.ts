96995ed2c4ed5338a33d84ed3020117f467d60d5
Thomas Steinke <exyphnos@gmail.com>
2021-12-07 22:06:20 -0800

    Probably as graceful as I can get the original solution

--- 2021/8/code.ts
+++ 2021/8/code.ts
@@ -1,142 +1,56 @@
 import * as fs from 'fs';
-import { Map, MapFromInput } from '../../map';
-// import { MakeGrid, MakeRow } from '../../makegrid.js';
-import { permute, gcd, lcm, range } from '../../utils';
-import { question } from 'readline-sync';
-const md5 = require('../../md5');
-const print = console.log;
+import { range } from '../../utils';
 
 let file = process.argv[2] || 'input';
 let raw = fs.readFileSync(file + '.txt').toString().trim();
-
 let asLines = raw.split('\n').map(line => line.trim());
-let asNumbers = raw.split('\n').map(line => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map(line =>
-    line.trim().split('\n').map(line =>
-        line.trim()));
-let asMap = MapFromInput('.');
-let asNumberMap = MapFromInput(0, s => parseInt(s))
 
-const numToDigit = {
-    2: [1],
-    3: 7,
-    4: 4,
-    5: 5,
-    6: 6,
+function intersect(string: string, include: string) {
+    return string.split('').filter(l => include.includes(l)).join('');
 }
 
-let result = 0;
+let part1 = 0;
+let part2 = 0;
 asLines.forEach(line => {
-    const [left, right] = line.split(' | ');
-    const lWords = left.split(' ').map(w => w.split('').sort().join(''));
-    const rWords = right.split(' ').map(w => w.split('').sort().join(''));
+    const [seed, output] = line
+        .split(' ')
+        .map(word => word.split('').sort().join(''))
+        .join(' ')
+        .split(' | ')
+        .map(side => side.split(' '));
 
-    let figuredOut = range(10).map(() => false);
-    let mapping = {};
+    const ofLength = range(8)
+        .map(length => seed.filter(word => word.length === length));
 
-    let is0, is1, is2, is3, is4, is5, is6, is7, is8, is9;
+    const lookup: string[] = range(10).map(() => '');
+    lookup[1] = ofLength[2][0];
+    lookup[4] = ofLength[4][0];
+    lookup[7] = ofLength[3][0];
+    lookup[8] = ofLength[7][0];
+    part1 += output.filter(word => lookup.indexOf(word) >= 0).length;
 
-    // 1
-    lWords.forEach(word => {
-        if (word.length === 2) {
-            is1 = word;
-        }
-    })
+    // 3 matches 1
+    lookup[3] = ofLength[5].filter(word => intersect(word, lookup[1]) === lookup[1])[0];
 
-    // 7
-    let A, B, C, D, E, F, G;
-    lWords.forEach(word => {
-        if (word.length === 3) {
-            A = word.split('').filter(a => a !== is1[0] && a !== is1[1])[0]
-        }
-    })
+    // 2 only has 2 sides in common with 4
+    lookup[2] = ofLength[5].filter(word => intersect(word, lookup[4]).length === 2)[0];
 
-    // 4
-    let BorD;
-    lWords.forEach(word => {
-        if (word.length === 4) {
-            is4 = word;
-            BorD = word.split('').filter(l => is1.indexOf(l) < 0)
-        }
-    })
+    // 5 is the last one
+    lookup[5] = ofLength[5].filter(word => word !== lookup[2] && word !== lookup[3])[0];
 
-    // 3
-    lWords.forEach(word => {
-        if (word.length === 5) {
-            if (word.includes(is1[0]) && word.includes(is1[1])) {
-                // 3
-                word.split('').forEach(l => {
-                    if (l === is1[0] || l === is1[1] || l === A) return;
-                    if (BorD.includes(l)) {
-                        D = l;
-                    }
-                    else {
-                        G = l;
-                    }
-                })
-            }
-        }
-    })
+    // 9 matches 4
+    lookup[9] = ofLength[6].filter(word => intersect(word, lookup[4]) === lookup[4])[0];
 
-    B = BorD.filter(i => i !== D)[0];
+    // 6 does NOT match 1
+    lookup[6] = ofLength[6].filter(word => intersect(word, lookup[1]) !== lookup[1])[0];
 
-    // 5
-    lWords.forEach(word => {
-        if (word.length === 5) {
-            if (!word.includes(is1[0]) || !word.includes(is1[1])) {
-                // 5
-                if (word.includes(B)) {
-                    F = word.split('').filter(l => [A, B, D, G].indexOf(l) < 0)[0]
-                    C = is1.split('').filter(l => l !== F)[0]
-                }
-            }
-        }
-    })
+    // 0 is the last one
+    lookup[0] = ofLength[6].filter(word => word !== lookup[6] && word !== lookup[9])[0];
 
-    // 8
-    lWords.forEach(word => {
-        if (word.length === 7) {
-            E = word.split('').filter(l => [A, B, C, D, F, G].indexOf(l) < 0)[0]
-        }
-    })
-
-    let num = 0;
-
-    const lookup = [
-        [A, B, C, E, F, G].sort().join(''),
-        [C, F].sort().join(''),
-        [A, C, D, E, G].sort().join(''),
-        [A, C, D, F, G].sort().join(''),
-        [B, C, D, F].sort().join(''),
-        [A, B, D, F, G].sort().join(''),
-        [A, B, D, E, F, G].sort().join(''),
-        [A, C, F].sort().join(''),
-        [A, B, C, D, E, F, G].sort().join(''),
-        [A, B, C, D, F, G].sort().join(''),
-    ]
-
-    rWords.forEach(word => {
-        num *= 10;
-        num += lookup.indexOf(word);
-    })
-
-    result += num;
-
-    // while (figuredOut.includes(false)) {
-    //     lWords.forEach(word => {
-    //         if (word.length === 2) {
-
-    //         }
-    //     })
-    // }
-
-    // rWords.forEach(word => {
-    //     if ([2, 4, 3, 7].indexOf(word.length) >= 0) {
-    //         result++;
-    //     }
-    // })
+    part2 += output
+        .map(word => lookup.indexOf(word))
+        .reduce((prev, val) => prev * 10 + val, 0);
 })
 
-print(result);
-
-
+console.log(`Part 1:`, part1);
+console.log(`Part 2:`, part2);
