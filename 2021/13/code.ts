115d02e36bb21899c1f0caa43b6d5179a8095d1c
Thomas Steinke <exyphnos@gmail.com>
2021-12-12 21:31:08 -0800

    Cleaned it up and made the font nice and pretty

--- 2021/13/code.ts
+++ 2021/13/code.ts
@@ -1,85 +1,60 @@
 import * as fs from 'fs';
-import { Map, MapFromInput } from '../../map';
-// import { MakeGrid, MakeRow } from '../../makegrid.js';
-import { permute, gcd, lcm, makeInt, range } from '../../utils';
-import { question } from 'readline-sync';
-const md5 = require('../../md5');
-const print = console.log;
+import { Map } from '../../map';
+import { addAll, makeInt } from '../../utils';
 
 let file = process.argv[2] || 'input';
 let raw = fs.readFileSync(file + '.txt').toString().trim();
-
-let asLines = raw.split('\n').map(line => line.trim());
-let asNumbers = raw.split('\n').map(line => parseInt(line.trim()));
 let asGroups = raw.split(/\r?\n\r?\n/).map(line =>
     line.trim().split('\n').map(line =>
         line.trim()));
-let asMap = MapFromInput('.');
-let asNumberMap = MapFromInput(0, makeInt)
 
-const paper = new Map('.');
-
+const paper = new Map(0);
 asGroups[0].forEach(line => {
     const [x, y] = line.split(',').map(makeInt);
-    paper.set(x, y, '#');
-})
+    paper.set(x, y, 1);
+});
 
-const folds = []
-asGroups[1].forEach(line => {
-    const axis = line[11];
-    const val = parseInt(line.substr(13));
-    folds.push({ axis, val });
-})
-
-function foldX(val: number) {
-    for (let x = val; x < paper.max.x; x++) {
-        let d = (x - val);
-        let destX = val - d;
-
+function foldX(value: number) {
+    for (let x = value; x < paper.max.x; x++) {
         for (let y = paper.min.y; y < paper.max.y; y++) {
-            if (paper.get(x, y) === '#') {
+            if (paper.has(x, y)) {
+                let destX = value - (x - value);
                 paper.set(destX, y, paper.get(x, y));
-                paper.set(x, y, '.');
             }
         }
     }
-    paper.max.x = val;
+    paper.max.x = value;
 }
 
-
-function foldY(val: number) {
-    for (let y = val; y < paper.max.y; y++) {
-        let d = (y - val);
-        let destY = val - d;
-
+function foldY(value: number) {
+    for (let y = value; y < paper.max.y; y++) {
         for (let x = paper.min.x; x < paper.max.x; x++) {
-            if (paper.get(x, y) === '#') {
-                print(x, y, 'goes to', x, destY);
+            if (paper.has(x, y)) {
+                let destY = value - (y - value);
                 paper.set(x, destY, paper.get(x, y));
-                paper.set(x, y, '.');
             }
         }
     }
-    paper.max.y = val;
+    paper.max.y = value;
 }
 
-function fold({ axis, val }) {
+function fold(axis: string, value: number) {
     if (axis === 'x') {
-        foldX(val);
+        foldX(value);
     }
     else {
-        foldY(val);
+        foldY(value);
     }
 }
 
+asGroups[1].forEach((line, i) => {
+    const [_, axis, value] = line.match(/fold along (.)=(\d+)/);
 
+    fold(axis, parseInt(value));
 
-// print(paper.print());
-folds.forEach(f => {
-    fold(f);
-    print()
+    if (i === 0) {
+        console.log(`Part 1`, paper.reduce(addAll, 0));
+    }
 })
-// fold(folds[0]);
 
-print(paper.reduce((p, v) => p + (v === '#' ? 1 : 0), 0));
-print(paper.print());
+console.log(paper.map(i => i ? '##' : '  ').print());
