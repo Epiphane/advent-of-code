4583c2e9f41f3fb30159ff7ff7fc44aa9fea4567
Thomas Steinke <tsteinke@riotgames.com>
2021-12-15 22:57:04 -0800

    Prettified

--- 2021/16/code.ts
+++ 2021/16/code.ts
@@ -1,18 +1,5 @@
 import * as fs from "fs";
-import { Map, MapFromInput } from "../../map";
-// import { MakeGrid, MakeRow } from '../../makegrid.js';
-import {
-  permute,
-  gcd,
-  lcm,
-  makeInt,
-  range,
-  addAll,
-  multiplyAll,
-} from "../../utils";
-import { question } from "readline-sync";
-const md5 = require("../../md5");
-const print = console.log;
+import { addAll, multiplyAll } from "../../utils";
 
 let file = process.argv[2] || "input";
 let raw = fs
@@ -20,124 +7,124 @@ let raw = fs
   .toString()
   .trim();
 
-let asLines = raw.split("\n").map((line) => line.trim());
-let asNumbers = raw.split("\n").map((line) => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map((line) =>
-  line
-    .trim()
-    .split("\n")
-    .map((line) => line.trim())
-);
-let asMap = MapFromInput(".");
-let asNumberMap = MapFromInput(0, makeInt);
+class Packet {
+  subpackets: Packet[] = [];
+  value?: number;
 
-let binary = raw
-  .trim()
-  .split("")
-  .map((w) => {
-    return parseInt(w, 16).toString(2).padStart(4, "0");
-  })
-  .join("");
+  constructor(public version: number, public type: number) {}
 
-function parseLiteral(i: number) {
-  //   while (i % 4 !== 0) {
-  //     i++;
-  //   }
-
-  let num = "";
-  let goagain = true;
-  while (goagain) {
-    goagain = binary[i] === "1";
-    i++;
-    num += binary.substr(i, 4);
-    i += 4;
+  versionSum() {
+    return (
+      this.version +
+      this.subpackets.reduce((prev, packet) => prev + packet.versionSum(), 0)
+    );
   }
 
-  //   while (i % 4 !== 0) {
-  //     i++;
-  //   }
-
-  console.log(
-    `Literal: ${parseInt(num, 2)} next i=${i} => ${binary.substr(i, 25)}`
-  );
-  return {
-    i,
-    val: parseInt(num, 2),
-  };
-}
-
-// binary = "00111000000000000110111101000101001010010001001000000000";
-
-let versions = 0;
-function parseOp(type, i: number) {
-  const lenTypeId = binary[i++];
-  let lenBits, subPacks;
-  const subpacks = [];
-  if (lenTypeId === "0") {
-    lenBits = parseInt(binary.substr(i, 15), 2);
-    // console.log(i, `LenBits: ${lenBits} (${binary.substr(i, 15)})`);
-    i += 15;
-    let dest = i + lenBits;
-
-    let val = 0;
-    while (i + 6 < dest) {
-      const { i: newI, val: newVal } = parse(i);
-      i = newI;
-      subpacks.push(newVal);
-      //   console.log(i, dest, binary.length);
-    }
-  } else {
-    subPacks = parseInt(binary.substr(i, 11), 2);
-    i += 11;
-    let val = 0;
-
-    // console.log(`Subpacks: ${subPacks}`);
-    for (let t = 0; t < subPacks; t++) {
-      const { i: newI, val: newVal } = parse(i);
-      i = newI;
-      subpacks.push(newVal);
+  getValue() {
+    const values = this.subpackets.map((packet) => {
+      return packet.getValue();
+    });
+    switch (this.type) {
+      case 0:
+        return values.reduce(addAll, 0);
+      case 1:
+        return values.reduce(multiplyAll, 1);
+      case 2:
+        return Math.min(...values);
+      case 3:
+        return Math.max(...values);
+      case 4:
+        return this.value!;
+      case 5:
+        return values[0] > values[1] ? 1 : 0;
+      case 6:
+        return values[0] < values[1] ? 1 : 0;
+      case 7:
+        return values[0] === values[1] ? 1 : 0;
     }
   }
+}
 
-  if (type === 0) {
-    return { i, val: subpacks.reduce(addAll, 0) };
-  } else if (type === 1) {
-    return { i, val: subpacks.reduce(multiplyAll, 1) };
-  } else if (type === 2) {
-    return { i, val: Math.min(...subpacks) };
-  } else if (type === 3) {
-    return { i, val: Math.max(...subpacks) };
-  } else if (type === 5) {
-    return { i, val: subpacks[0] > subpacks[1] ? 1 : 0 };
-  } else if (type === 6) {
-    return { i, val: subpacks[0] < subpacks[1] ? 1 : 0 };
-  } else if (type === 7) {
-    return { i, val: subpacks[0] === subpacks[1] ? 1 : 0 };
+class PacketReader {
+  binary: string;
+  cursor = 0;
+
+  versionTotal = 0;
+  packet: Packet;
+
+  constructor(transmissionHex: string) {
+    this.binary = transmissionHex
+      .split("")
+      .map((w) => {
+        return parseInt(w, 16).toString(2).padStart(4, "0");
+      })
+      .join("");
+
+    this.packet = this.parse();
+  }
+
+  read(len: number, verbose = false) {
+    if (verbose) {
+      console.log(this.binary.substr(this.cursor, len));
+    }
+    const value = parseInt(this.binary.substr(this.cursor, len), 2);
+    this.cursor += len;
+    return value;
+  }
+
+  parseLiteral() {
+    let value = 0;
+    let continueParsing: number;
+    do {
+      continueParsing = this.read(1);
+      value *= 16;
+      value += this.read(4);
+    } while (continueParsing);
+
+    return value;
+  }
+
+  parseSubpackets() {
+    const type = this.read(1);
+    const packets: Packet[] = [];
+    if (type === 0) {
+      const numBits = this.read(15);
+      const dest = this.cursor + numBits;
+      while (this.cursor < dest) {
+        packets.push(this.parse());
+      }
+    } else {
+      const numPackets = this.read(11);
+      for (let n = 0; n < numPackets; n++) {
+        packets.push(this.parse());
+      }
+    }
+
+    return packets;
+  }
+
+  parse() {
+    if (this.cursor + 4 >= this.binary.length) {
+      this.cursor = this.binary.length;
+      return;
+    }
+
+    const version = this.read(3);
+    const type = this.read(3);
+
+    this.versionTotal += version;
+
+    const packet = new Packet(version, type);
+    if (type === 4) {
+      packet.value = this.parseLiteral();
+    } else {
+      packet.subpackets = this.parseSubpackets().filter((p) => !!p);
+    }
+
+    return packet;
   }
 }
 
-function parse(i: number) {
-  if (i + 6 >= binary.length) {
-    return { i, val: 0 };
-  }
-
-  let version = parseInt(binary.substr(i, 3), 2);
-  let type = parseInt(binary.substr(i + 3, 3), 2);
-
-  //   print(version, type);
-  print(i, `version`, version, `type`, type);
-  versions += version;
-
-  if (type === 4) {
-    return parseLiteral(i + 6);
-  } else {
-    return parseOp(type, i + 6);
-  }
-}
-
-// print(version, type);
-
-print(binary);
-print(binary.length);
-print(versions);
-print(parse(0));
+const reader = new PacketReader(raw);
+console.log(`Part 1`, reader.versionTotal);
+console.log(`Part 2`, reader.packet.getValue());
