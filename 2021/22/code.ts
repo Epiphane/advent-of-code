a84981883eb95d3b5969d9a59b7ddead2a4593af
Thomas Steinke <exyphnos@gmail.com>
2021-12-21 23:25:17 -0800

    Tidying up day 21

--- 2021/22/code.ts
+++ 2021/22/code.ts
@@ -1,242 +1,104 @@
 import * as fs from 'fs';
-import { Map, MapFromInput } from '../../map';
-import { permute, gcd, lcm, makeInt, range, addAll } from '../../utils';
-import { question } from 'readline-sync';
 import { Point } from '../../point';
-const md5 = require('../../md5');
-const print = console.log;
 
 let file = process.argv[2] || 'input';
 let raw = fs.readFileSync(file + '.txt').toString().trim();
-
 let asLines = raw.split('\n').map(line => line.trim());
-let asNumbers = raw.split('\n').map(line => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map(line =>
-    line.trim().split('\n').map(line =>
-        line.trim()));
-let asNumberMap = MapFromInput(0, makeInt)
 
-// let cuboids = [];
+class Block {
+    constructor(
+        public on: boolean,
+        public min: Point,
+        public max: Point,
+    ) { }
 
-// range(101).forEach(x => {
-//     x -= 50
-//     const rx = [];
-//     range(101).forEach(y => {
-//         y -= 50;
-//         const ry =[];
-//          range(101).forEach(z => {
-//             z -= 50;
-//             ry[z] = 0;
-//         })
-//         rx[y] = ry;
-//     })
-//     cuboids[x] = rx;
-// });
-
-// cuboids = range(101).map(() => range(101).map(() => range(101).map(() => 0)))
-
-let cuboids = {};
-
-type Block = {
-    on: boolean;
-    min: Point;
-    max: Point;
-}
-
-let blocks = [] as Block[];
-
-function loggy(block: Block) {
-    return `${block.on ? 'on' : 'off'} {${block.min.x},${block.min.y},${block.min.z}} {${block.max.x - 1},${block.max.y - 1},${block.max.z - 1}}`;
-}
-
-asLines.forEach(line => {
-    const [dir, splits] = line.split(' ');
-    const [xs, ys, zs] = splits.split(',').map(p => {
-        return p.substring(2).split('..').map(makeInt);
-    })
-
-    const min = new Point(xs[0], ys[0], zs[0]);
-    const max = new Point(xs[1] + 1, ys[1] + 1, zs[1] + 1);
-
-    console.log('\n\n\n\n\n\n\n');
-    console.log(dir, min, max);
-
-    const newb = {
-        on: dir === 'on', min, max
-    };
-
-    // if (!newb.on) console.log(blocks.map(loggy).join('\n'));
-
-    // Split up overlaps
-    for (let index = 0; index < blocks.length; index++) {
-        const element = blocks[index];
-
-        // if (!newb.on) console.log('compare', index, loggy(element), 'to', loggy(newb));
-        if (element.max.x < min.x ||
-            element.min.x > max.x ||
-            element.max.y < min.y ||
-            element.min.y > max.y ||
-            element.max.z < min.z ||
-            element.min.z > max.z) {
-            // console.log('  outta there');
-            continue;
-        }
-
-        // Split in half
-        if (min.x < element.max.x && min.x > element.min.x) {
-            let newMin = new Point(min.x, element.min.y, element.min.z);
-            let newMax = new Point(element.max.x, element.max.y, element.max.z);
-            element.max.x = min.x;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's1\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-        if (max.x >= element.min.x && max.x < element.max.x) {
-            let newMin = new Point(element.min.x, element.min.y, element.min.z);
-            let newMax = new Point(max.x, element.max.y, element.max.z);
-            element.min.x = max.x;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's2\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-
-        // Split y
-        if (min.y < element.max.y && min.y > element.min.y) {
-            let newMin = new Point(element.min.x, min.y, element.min.z);
-            let newMax = new Point(element.max.x, element.max.y, element.max.z);
-            element.max.y = min.y;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's3\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-        if (max.y > element.min.y && max.y < element.max.y) {
-            let newMin = new Point(element.min.x, element.min.y, element.min.z);
-            let newMax = new Point(element.max.x, max.y, element.max.z);
-            element.min.y = max.y;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's4\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-
-        // Split z
-        if (min.z < element.max.z && min.z > element.min.z) {
-            let newMin = new Point(element.min.x, element.min.y, min.z);
-            let newMax = new Point(element.max.x, element.max.y, element.max.z);
-            element.max.z = min.z;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's5\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-        if (max.z > element.min.z && max.z < element.max.z) {
-            let newMin = new Point(element.min.x, element.min.y, element.min.z);
-            let newMax = new Point(element.max.x, element.max.y, max.z);
-            element.min.z = max.z;
-
-            blocks.push({
-                on: element.on,
-                min: newMin,
-                max: newMax,
-            });
-            // if (!newb.on) console.log(index, 's6\n', loggy(element), '\n', loggy(blocks[blocks.length - 1]));
-        }
-
-        if (max.x >= element.max.x &&
-            max.y >= element.max.y &&
-            max.z >= element.max.z &&
-            min.x <= element.min.x &&
-            min.y <= element.min.y &&
-            min.z <= element.min.z) {
-            // if (!newb.on) console.log('removing', element, 'cuz', min, max);
-            blocks.splice(index--, 1);
-        }
+    area() {
+        return this.max.sub(this.min).area();
     }
 
-    blocks.push(newb);
-})
-
-// let result = 0;
-
-// for (let i = 0; i < blocks.length; i++) {
-//     const block = blocks[i];
-
-//     for (let j = 0; j < i; j++) {
-//         const other = blocks[j];
-
-//         if (!other.on) {
-//             continue;
-//         }
-
-//         if (other.max.x < block.min.x ||
-//             other.max.y < block.min.y ||
-//             other.max.z < block.min.z ||
-//             block.max.x < other.min.x ||
-//             block.max.y < other.min.y ||
-//             block.max.z < other.min.z) {
-//             continue;
-//         }
-
-//         const minOverlap = new Point(
-//             Math.min(block.max.x, other.max.x),
-//             Math.min(block.max.y, other.max.y),
-//             Math.min(block.max.z, other.max.z),
-//         );
-//         const maxOverlap = new Point(
-//             Math.max(block.min.x, other.min.x),
-//             Math.max(block.min.y, other.min.y),
-//             Math.max(block.min.z, other.min.z),
-//         );
-
-
-//     }
-// }
-
-let result = 0;
-
-console.log()
-console.log(blocks.map(loggy).join('\n'));
-
-// console.log(blocks);
-blocks.sort((b1, b2) => {
-    if (b1.min.x !== b2.min.x) return b1.min.x - b2.min.x;
-    if (b1.min.y !== b2.min.y) return b1.min.y - b2.min.y;
-    if (b1.min.z !== b2.min.z) return b1.min.z - b2.min.z;
-    if (b1.max.x !== b2.max.x) return b1.max.x - b2.max.x;
-    if (b1.max.y !== b2.max.y) return b1.max.y - b2.max.y;
-    if (b1.max.z !== b2.max.z) return b1.max.z - b2.max.z;
-})
-
-blocks.forEach((block, i) => {
-    console.log(i, block.on, block.min, block.max, (block.max.x - block.min.x) * (block.max.y - block.min.y) * (block.max.z - block.min.z))
-    if (block.on) {
-        result += (block.max.x - block.min.x) * (block.max.y - block.min.y) * (block.max.z - block.min.z);
+    copy() {
+        return new Block(this.on, this.min.copy(), this.max.copy());
     }
-})
+}
 
-// for (let i in cuboids) {
-//     result += cuboids[i];
-// }
+const input = asLines.map(line => {
+    const [_, dir, x0, x1, y0, y1, z0, z1] = line
+        .match(/(\w+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/);
 
-console.log(result);
+    return new Block(
+        dir === 'on',
+        new Point(x0, y0, z0),
+        new Point(x1, y1, z1).add(new Point(1, 1, 1)),
+    );
+});
 
+function CalculateLitArea(blocks: Block[]) {
+    const disjoint: Block[] = [];
 
-// console.log(cuboids.reduce((prev, rx) => prev + rx.reduce((p2, ry) => p2 + ry.reduce(addAll, 0), 0), 0));
+    blocks.forEach(block => {
+        block = block.copy();
+
+        for (let i = 0; i < disjoint.length; i++) {
+            const other = disjoint[i];
+
+            if (other.max.x < block.min.x ||
+                other.min.x > block.max.x ||
+                other.max.y < block.min.y ||
+                other.min.y > block.max.y ||
+                other.max.z < block.min.z ||
+                other.min.z > block.max.z) {
+                continue;
+            }
+
+            // Split up overlaps
+            ['x', 'y', 'z'].forEach(c => {
+                if (other.min[c] < block.min[c] && block.min[c] < other.max[c]) {
+                    const split = other.copy();
+                    split.min[c] = other.max[c] = block.min[c];
+                    disjoint.push(split);
+                }
+
+                if (other.min[c] < block.max[c] && block.max[c] < other.max[c]) {
+                    const split = other.copy();
+                    split.max[c] = other.min[c] = block.max[c];
+                    disjoint.push(split);
+                }
+            });
+
+            // Consume this block if it's redundant
+            if (block.max.x >= other.max.x &&
+                block.max.y >= other.max.y &&
+                block.max.z >= other.max.z &&
+                block.min.x <= other.min.x &&
+                block.min.y <= other.min.y &&
+                block.min.z <= other.min.z) {
+                disjoint.splice(i--, 1);
+            }
+        }
+
+        disjoint.push(block);
+    });
+
+    return disjoint.filter(block => block.on).reduce((prev, block) => prev + block.area(), 0);
+}
+
+// Stupid funny solution: turn off everything that's outside -50,-50,-50
+const min = new Point(
+    Math.min(...input.map(b => b.min.x)),
+    Math.min(...input.map(b => b.min.y)),
+    Math.min(...input.map(b => b.min.z))
+);
+const max = new Point(
+    Math.max(...input.map(b => b.max.x)),
+    Math.max(...input.map(b => b.max.y)),
+    Math.max(...input.map(b => b.max.z))
+);
+console.log(`Part 1:`, CalculateLitArea(input.concat([
+    new Block(false, new Point(50, min.y, min.z), new Point(max.x, max.y, max.z)),
+    new Block(false, new Point(min.x, 50, min.z), new Point(max.x, max.y, max.z)),
+    new Block(false, new Point(min.x, min.y, 50), new Point(max.x, max.y, max.z)),
+    new Block(false, new Point(min.x, min.y, min.z), new Point(-50, max.y, max.z)),
+    new Block(false, new Point(min.x, min.y, min.z), new Point(max.x, -50, max.z)),
+    new Block(false, new Point(min.x, min.y, min.z), new Point(max.x, max.y, -50)),
+])));
+console.log(`Part 2:`, CalculateLitArea(input));
--- point.ts
+++ point.ts
@@ -1,9 +1,17 @@
 export class Point {
+    x: number;
+    y: number;
+    z: number;
+
     constructor(
-        public x: number,
-        public y: number,
-        public z: number = 0
-    ) { }
+        x: string | number,
+        y: string | number,
+        z: string | number = 0
+    ) {
+        this.x = (typeof (x) === 'string') ? parseInt(x) : x;
+        this.y = (typeof (y) === 'string') ? parseInt(y) : y;
+        this.z = (typeof (z) === 'string') ? parseInt(z) : z;
+    }
 
     sub(other: Point) {
         return new Point(this.x - other.x, this.y - other.y, this.z - other.z);
@@ -15,5 +23,13 @@ export class Point {
 
     equals(other: Point) {
         return this.x === other.x && this.y === other.y && this.z === other.z;
+    }
+
+    copy() {
+        return new Point(this.x, this.y, this.z);
+    }
+
+    area() {
+        return this.x * this.y * this.z;
     }
 }
