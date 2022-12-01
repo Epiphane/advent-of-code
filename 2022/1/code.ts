49144366a3e7a935ec7b5bf31baff123b89364be
Thomas Steinke <exyphnos@gmail.com>
2022-11-30 21:09:57 -0800

    Day 1 polished

--- 2022/1/code.ts
+++ 2022/1/code.ts
@@ -1,54 +1,17 @@
 // @ts-nocheck
 
 import * as fs from 'fs';
-import { Map, MapFromInput } from '../../map';
-import { permute, gcd, lcm, makeInt, range, mode, descending } from '../../utils';
-import { question } from 'readline-sync';
-const md5 = require('../../md5');
+import { addAll, descending } from '../../utils';
 const print = console.log;
 
 let file = process.argv[2] || 'input';
 let raw = fs.readFileSync(file + '.txt').toString().trim();
 
-let asLines = raw.split('\n').map(line => line.trim());
-let asNumbers = raw.split('\n').map(line => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map(line =>
+let input = raw.split(/\r?\n\r?\n/).map(line =>
     line.trim().split('\n').map(line =>
-        line.trim()));
-let asMap = MapFromInput('.');
-let asNumberMap = MapFromInput(0, makeInt)
-let asWords = raw.split(',');
+        parseInt(line.trim())));
 
-let total = 0;
-
-let max = 0;
-let weights = [];
-
-asGroups.forEach(group => {
-    let w = 0;
-    group.forEach(line => w += parseInt(line));
-    max = Math.max(max, w);
-    weights.push(w);
-})
-
-
-weights.sort(descending);
-
-print(weights[0] + weights[1] + weights[2])
-
-print(max);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// print(total);
+const elves = input.map(elf => elf.reduce(addAll, 0));
+elves.sort(descending);
+print(`Part 1: ${elves[0]}`);
+print(`Part 2: ${elves.slice(0, 3).reduce(addAll, 0)}`);
