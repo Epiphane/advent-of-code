7e636036a6a4b0e8caa56a9e6e79c09009ec44dc
Thomas Steinke <exyphnos@gmail.com>
2022-12-06 21:31:53 -0800

    Day 7 cleanup

--- 2022/7/code.ts
+++ 2022/7/code.ts
@@ -1,111 +1,75 @@
-// @ts-nocheck
-
 import * as fs from 'fs';
-import { Interpreter } from '../../interpreter.ts';
-import { Map, MapFromInput } from '../../map';
-import { permute, gcd, lcm, makeInt, range, mode } from '../../utils';
-import { question } from 'readline-sync';
-const md5 = require('../../md5');
 const print = console.log;
-
 let file = process.argv[2] || 'input';
-let raw = fs.readFileSync(file + '.txt').toString().trim();
 
-let asLines = raw.split('\n').map(line => line.trim());
-let asNumbers = raw.split('\n').map(line => parseInt(line.trim()));
-let asGroups = raw.split(/\r?\n\r?\n/).map(line =>
-    line.trim().split('\n').map(line =>
-        line.trim()));
-let asMap = MapFromInput('.');
-let asNumberMap = MapFromInput(0, makeInt)
+const directories: Entry[] = [];
+class Entry {
+    fileSize?: number;
+    children: Entry[] = [];
 
-let total = 0;
-
-let entries = {};
-let files = {};
-
-let path = '/';
-let listing = false;
-const makeDir = (parent, path) => {
-    return { path, children: [], parent, size: -1 };
-}
-const makeFile = (parent, path, size) => {
-    return { path, children: [], parent, size };
-}
-function getSize(file) {
-    if (file.size >= 0) return file.size;
-    return file.children.reduce((prev, f) => prev + getSize(f), 0);
-}
-let cursor = makeDir(null, '/');
-const top = cursor
-let dirs = [];
-asLines.forEach(line => {
-    if (line[0] === '$') {
-        const [_, cm, pt] = line.split(' ');
-        switch (cm) {
-            case 'cd':
-                if (pt === '/') {
-                    cursor = top;
-                }
-                else if (pt === '..') {
-                    cursor = cursor.parent;
-                }
-                else {
-                    path += pt;
-                    cursor = cursor.children.find(c => c.path === pt);
-                }
-                break;
-            case 'ls':
-                break;
-        }
-    }
-    else {
-        let words = line.split(' ');
-        if (words[0] === 'dir') {
-            // dir
-            let dn = words[1]
-            const d = makeDir(cursor, dn);
-            cursor.children.push(d);
-            dirs.push(d);
+    constructor(
+        public readonly parent: Entry,
+        public readonly name: string,
+        size?: number
+    ) {
+        if (size) {
+            this.fileSize = size;
         }
         else {
-            let sz = parseInt(words[0]);
-            // total += sz;
-            let name = words[1];
-            cursor.children.push(makeFile(cursor, name, sz));
+            directories.push(this);
         }
     }
-})
 
-total = 0;
+    get size() {
+        if (this.fileSize) {
+            return this.fileSize;
+        }
 
-dirs.forEach(dir => {
-    const s = getSize(dir);
-    if (s <= 100000) {
-        total += s;
-        // print(dir, s);
+        return this.children.reduce(
+            (prev, child) => prev + child.size, 0
+        );
     }
-})
-print(total);
+}
 
-dirs.forEach(d => {
-    d.size = getSize(d);
-})
+const root = new Entry(null, '/');
 
-let best = 10000000000;
+let cursor = root;
+fs.readFileSync(file + '.txt').toString()
+    .trim()
+    .split('\n')
+    .map(line => line.trim().split(' '))
+    .forEach(words => {
+        if (words[0] === '$') {
+            if (words[1] === 'cd') {
+                switch (words[2]) {
+                    case '/':
+                        cursor = root;
+                        break;
+                    case '..':
+                        cursor = cursor.parent;
+                        break;
+                    default:
+                        cursor = cursor.children.find(child => child.name === words[2]);
+                        break;
+                }
+            }
+        }
+        else {
+            if (words[0] === 'dir') {
+                cursor.children.push(new Entry(cursor, words[1]));
+            }
+            else {
+                cursor.children.push(new Entry(cursor, words[1], parseInt(words[0])));
+            }
+        }
+    });
 
-let used = getSize(top);
-let unused = 70000000 - used;
+print(`Part 1:`, directories
+    .filter(dir => dir.size <= 100000)
+    .reduce((prev, dir) => prev + dir.size, 0));
 
-dirs.forEach(dir => {
-    if (unused + dir.size >= 30000000) {
-        best = Math.min(dir.size, best);
-        print(dir.size);
-    }
-})
-print(unused);
-print(best, unused + best);
-
-// print(top);
-// print(dirs[0]);
-// print(getSize(dirs[0]));
+const unused = 70000000 - root.size;
+const needed = 30000000;
+print(`Part 2:`, Math.min(...directories
+    .filter(dir => unused + dir.size >= needed)
+    .map(dir => dir.size)));
