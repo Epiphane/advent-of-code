e33f01440fde19063253d2050129bcfeda7b6ef8
Thomas Steinke <exyphnos@gmail.com>
2023-12-05 21:07:32 -0800

    off by one whyyyyyyyy

--- 2023/6/code.ts
+++ 2023/6/code.ts
@@ -3,7 +3,7 @@
 import * as fs from 'fs';
 import { Interpreter } from '../../interpreter.ts';
 import { Map, MapFromInput } from '../../map';
-import { permute, gcd, lcm, makeInt, range, mode } from '../../utils';
+import { permute, gcd, lcm, makeInt, range, mode, multiplyAll } from '../../utils';
 import { question } from 'readline-sync';
 import { Point } from '../../point';
 const md5 = require('../../md5');
@@ -26,6 +26,27 @@ let asNumberMap = MapFromInput(0, makeInt)
 
 let total = 0;
 
-asLines.forEach(line => {
-    const [] = line.match(/foobar/);
-})
+let times = asLines[0].split(' ').slice(1).map(makeInt).filter(i => !isNaN(i));
+let distances = asLines[1].split(' ').slice(1).map(makeInt).filter(i => !isNaN(i));
+
+let nums = [];
+for (let i = 0; i < 1; i++) {
+    let num = 0;
+
+    print(times[i], distances[i])
+
+    for (let t = 0; t < times[i]; t++) {
+        let speed = t;
+
+        let finalDist = speed * (times[i] - t);
+        // print(t, distances[i], finalDist, finalDist > distances[i]);
+        if (finalDist > distances[i]) {
+            num++;
+        }
+    }
+
+    // nums.push(num.length);
+    print(num);
+}
+
+// print(nums.reduce(multiplyAll, 1))
